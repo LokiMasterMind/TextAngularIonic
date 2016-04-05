b5b50d2075a6e35c5a2af8b90499e5ffc054e806
LokiMasterMind <chandawarlokesh@gmail.com>
2016-04-05 23:26:09 +0530

    First commit

---
+++ js/controllers.js
@@ -0,0 +1,21 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function ($scope, $rootScope) {
+    $scope.orightml = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li style="color: green;">Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li class="text-danger">Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
+    $rootScope.htmlcontent = $scope.orightml;
+    $scope.disabled = false;
+})
+
+.controller('ChatsCtrl', function ($scope, $rootScope) {
+    $scope.htmlcontent = $rootScope.htmlcontent;
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
